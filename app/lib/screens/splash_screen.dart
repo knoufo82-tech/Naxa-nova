50de69dcc270254abc12d1d0144fb751f7acc0ed
knoufo82-tech <knoufo82@gmail.com>
2026-09-06 20:54:54 +0000

    Create splash_screen.dart

---
+++ app/lib/screens/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'NAXA NOVA',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
